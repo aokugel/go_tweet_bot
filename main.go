4dbe9a009670d8b83f478d40b460150459d763f1
anthony kugel <anthonykugel@gmail.com>
2022-09-22 13:05:01 -0500

    code now retweets cyclone larry's five most recent tweets

--- main.go
+++ main.go
@@ -56,54 +56,27 @@ func main() {
 		log.Println("Error getting Twitter Client")
 		log.Println(err)
 	}
-
-	// Print out the pointer to our client
-	// for now so it doesn't throw errors
-	// tweet, resp, err := client.Statuses.Update(tweetBody, nil)
-	// if err != nil {
-	// 	log.Println(err)
-	// }
-	// log.Printf("%v\n", resp)
-	// log.Printf("%+v\n", tweet)
-
-	// Home Timeline
-	homeTimelineParams := &twitter.HomeTimelineParams{
-		Count:     2,
-		TweetMode: "extended",
-	}
-	tweets, _, _ := client.Timelines.HomeTimeline(homeTimelineParams)
-	fmt.Printf("User's HOME TIMELINE:\n%+v\n", tweets)
-
-	// Mention Timeline
-	mentionTimelineParams := &twitter.MentionTimelineParams{
-		Count:     2,
-		TweetMode: "extended",
-	}
-	tweets, _, _ = client.Timelines.MentionTimeline(mentionTimelineParams)
-	fmt.Printf("User's MENTION TIMELINE:\n%+v\n", tweets)
-
-	// Retweets of Me Timeline
-	retweetTimelineParams := &twitter.RetweetsOfMeTimelineParams{
-		Count:     2,
-		TweetMode: "extended",
-	}
-	tweets, _, _ = client.Timelines.RetweetsOfMeTimeline(retweetTimelineParams)
-	fmt.Printf("User's 'RETWEETS OF ME' TIMELINE:\n%+v\n", tweets)
-
-	// Update (POST!) Tweet (uncomment to run)
-	// tweet, _, _ := client.Statuses.Update("just
 	larryTimeline := &twitter.UserTimelineParams{
-		Count:     10,
-		TweetMode: "extended",
-		UserID:    1168018744804691968,
+		Count:           5,
+		TweetMode:       "extended",
+		UserID:          1168018744804691968,
+		ExcludeReplies:  newFalse(),
+		IncludeRetweets: newFalse(),
 	}
-	tweets, _, _ = client.Timelines.UserTimeline(larryTimeline)
-	fmt.Printf("User's 'RETWEETS OF ME' TIMELINE:\n%+v\n", tweets)
-	fmt.Printf("%T \n", tweets)
+	tweets, _, _ := client.Timelines.UserTimeline(larryTimeline)
 
-	for _, tweet := range tweets {
-		// fmt.Println(index, tweet)
-		fmt.Printf("%v", tweet.Text)
+	for index, tweet := range tweets {
+		fmt.Printf("%v \n %v \n %T \n", index, tweet.FullText, tweet.FullText)
+		client.Statuses.Retweet(tweet.ID, nil)
 	}
 
+	fmt.Println(tweets[0].ID)
+
+	client.Statuses.Retweet(tweets[0].ID, nil)
+
+}
+
+func newFalse() *bool {
+	b := false
+	return &b
 }
