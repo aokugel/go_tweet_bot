063cb5d668063fbb749a142ffb4cd86946a7ce80
anthony kugel <anthonykugel@gmail.com>
2022-09-22 07:30:22 -0500

    Added functionality to pull user tweets

--- main.go
+++ main.go
@@ -56,14 +56,54 @@ func main() {
 		log.Println("Error getting Twitter Client")
 		log.Println(err)
 	}
-	tweetBody := os.Args[1]
+
 	// Print out the pointer to our client
 	// for now so it doesn't throw errors
-	tweet, resp, err := client.Statuses.Update(tweetBody, nil)
-	if err != nil {
-		log.Println(err)
+	// tweet, resp, err := client.Statuses.Update(tweetBody, nil)
+	// if err != nil {
+	// 	log.Println(err)
+	// }
+	// log.Printf("%v\n", resp)
+	// log.Printf("%+v\n", tweet)
+
+	// Home Timeline
+	homeTimelineParams := &twitter.HomeTimelineParams{
+		Count:     2,
+		TweetMode: "extended",
 	}
-	log.Printf("%v\n", resp)
-	log.Printf("%+v\n", tweet)
+	tweets, _, _ := client.Timelines.HomeTimeline(homeTimelineParams)
+	fmt.Printf("User's HOME TIMELINE:\n%+v\n", tweets)
+
+	// Mention Timeline
+	mentionTimelineParams := &twitter.MentionTimelineParams{
+		Count:     2,
+		TweetMode: "extended",
+	}
+	tweets, _, _ = client.Timelines.MentionTimeline(mentionTimelineParams)
+	fmt.Printf("User's MENTION TIMELINE:\n%+v\n", tweets)
+
+	// Retweets of Me Timeline
+	retweetTimelineParams := &twitter.RetweetsOfMeTimelineParams{
+		Count:     2,
+		TweetMode: "extended",
+	}
+	tweets, _, _ = client.Timelines.RetweetsOfMeTimeline(retweetTimelineParams)
+	fmt.Printf("User's 'RETWEETS OF ME' TIMELINE:\n%+v\n", tweets)
+
+	// Update (POST!) Tweet (uncomment to run)
+	// tweet, _, _ := client.Statuses.Update("just
+	larryTimeline := &twitter.UserTimelineParams{
+		Count:     10,
+		TweetMode: "extended",
+		UserID:    1168018744804691968,
+	}
+	tweets, _, _ = client.Timelines.UserTimeline(larryTimeline)
+	fmt.Printf("User's 'RETWEETS OF ME' TIMELINE:\n%+v\n", tweets)
+	fmt.Printf("%T \n", tweets)
+
+	for _, tweet := range tweets {
+		// fmt.Println(index, tweet)
+		fmt.Printf("%v", tweet.Text)
+	}
 
 }
